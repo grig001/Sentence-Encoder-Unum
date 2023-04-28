ad01247e485f9b7d6ccfa67dd2791d2bab8e197b
Grigor <78757814+grig001@users.noreply.github.com>
2023-04-28 21:48:18 +0400

    Add files via upload

--- My_dataset_script.py
+++ My_dataset_script.py
@@ -1,3 +1,5 @@
+import csv
+
 from datasets import Dataset
 from datasets import load_dataset
 
@@ -5,10 +7,10 @@ train_data = []
 dataset = load_dataset('bookcorpus', split='train', streaming=True)
 
 for i, item in enumerate(dataset):
-    if i >= 2e8:
+    if i >= 5e2:
         break
 
-    if i % 10 ** 6 == 0:
+    if i % 10**2 == 0:
         print(f'{i} iteration is done')
 
     train_data.append({'text': item['text']})
@@ -16,8 +18,8 @@ for i, item in enumerate(dataset):
 
 data = Dataset.from_list(train_data)
 
-file = open('data.txt', 'w')
-for item in data:
-    file.write(item['text'] + "\n")
+with open('my_data.csv', 'w') as file:
+    for item in data:
+        file.write(item['text'] + '\n')
 
 file.close()
